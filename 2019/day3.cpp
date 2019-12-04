69b7b47860f980381c33d32c84abec0ca921073d
Eduardo Garibay <dark.cloud2007@gmail.com>
2019-12-04 00:00:06 -0600

    Fix iteration counters and intersection function

--- 2019/day3.cpp
+++ 2019/day3.cpp
@@ -1,30 +1,32 @@
 #include "common.h"
+#include <algorithm>
 #include <sstream>
 #include <unordered_set>
 
-std::vector<std::string> intersection(std::vector<std::string> &v1, std::vector<std::string> &v2)
+std::vector<int *> intersection(std::vector<int *> &v1, std::vector<int *> &v2)
 {
-  std::unordered_set<std::string> m(v1.begin(),v1.end());
-  std::vector<std::string> v3;
+  std::vector<int *> v3;
 
-  for(auto a : v2)
-  {
-    if (m.count(a))
-    {
-      v3.push_back(a);
-      m.erase(a);
-    }
-  }
+  std::sort(v1.begin(), v1.end());
+  std::sort(v2.begin(), v2.end());
+
+  std::set_intersection(v1.begin(),v1.end(),
+    v2.begin(),v2.end(),
+    back_inserter(v3)
+  );
+
+  std::vector<int *>::iterator ip;
+  ip = std::unique(v3.begin(), v3.end());
+  v3.resize(std::distance(v3.begin(), ip));
 
   return v3;
 }
 
 int main()
 {
-  int x = { 0 };
-  int y = { 0 };
   std::ifstream fs;
-  std::string file = "day3.txt";
+  // std::string file = "day3.txt";
+  std::string file = "day3_test.txt";
   fs.open(file);
 
   if (!fs)
@@ -49,60 +51,73 @@ int main()
   }
 
   int vsSize = vs.size();
-  matrix wires;
+  std::vector<std::vector<int *> > wires;
   wires.reserve(1000);
   int px = { 0 };
   int py = { 0 };
-  int length = { 0 };
+
   for(int i=0; i < vsSize; i++)
   {
     px = 0;
     py = 0;
+
     for(int j=0; j < vs[i].size(); j++)
     {
       s = vs[i][j];
       std::string dir = s.substr(0,1);
       int dist = std::atoi(s.substr(1).c_str());
+
       if (dir == "U")
       {
         for(int k=0; k < dist; k++)
         {
-          std::string pos = std::to_string(px+1) + "," + std::to_string(py);
-          wires[i].push_back(pos);
+          int arr[2] = {px, py++};
+          wires[i].push_back(arr);
         }
       }
-      if (dir == "D")
+      else if (dir == "D")
       {
         for(int k=0; k < dist; k++)
         {
-          std::string pos = std::to_string(px-1) + "," + std::to_string(py);
-          wires[i].push_back(pos);
+          int arr[2] = {px, py--};
+          wires[i].push_back(arr);
         }
       }
-      if (dir == "L")
+      else if (dir == "R")
       {
         for(int k=0; k < dist; k++)
         {
-          std::string pos = std::to_string(px) + "," + std::to_string(py-1);
-          wires[i].push_back(pos);
+          int arr[2] = {px++, py};
+          wires[i].push_back(arr);
         }
       }
-      if (dir == "R")
+      else if (dir == "L")
       {
         for(int k=0; k < dist; k++)
         {
-          std::string pos = std::to_string(px) + "," + std::to_string(py+1);
-          wires[i].push_back(pos);
+          int arr[2] = {px--, py};
+          wires[i].push_back(arr);
         }
       }
+      else
+        continue; // skip
+      std::cout << px << "," << py << std::endl;
     }
+    std::cout << "====" << std::endl;
   }
 
-  std::vector<std::string> v3 = intersection(wires[0], wires[1]);
+  //p1
+  std::vector<int *> v3 = intersection(wires[0], wires[1]);
+  int output = { 0 };
   for(int i=0; i < v3.size(); i++)
   {
-    std::cout << v3[i] << std::endl;
+    std::cout << v3[i][0] << "," << v3[i][1] << std::endl;
+    int m_dist = v3[i][0] + v3[i][1];
+    // std::cout << m_dist << std::endl;
+    if (output == 0 || output > m_dist)
+      output = m_dist;
   }
 
+  std::cout << output << std::endl;
   return 0;
 }
