01e077a166af2ba6a19dfcc974176f7df7d41690
Eduardo Garibay <dark.cloud2007@gmail.com>
2019-12-04 22:23:09 -0600

    AoC 2019 Day 4 Part 2 solution

--- 2019/day4.cpp
+++ 2019/day4.cpp
@@ -1,7 +1,8 @@
 #include "common.h"
 #include <numeric>
+#include <map>
 
-void validateNumPass(std::vector<int>& pwCol, int pw)
+void validateNumPass(int& p1Size, int&p2Size, int pw)
 {
   int tmpNum = pw;
   int digit;
@@ -14,44 +15,81 @@ void validateNumPass(std::vector<int>& pwCol, int pw)
     vd.push_back(digit);
   }
 
-  bool isValid = false;
+  bool isAsc   = false;
+  bool isP1Val = false;
+  bool isP2Val = false;
   int prevDigi = -1;
+  std::map<int,int> freq_map;
+
   for(int i=0; i < vd.size(); i++)
   {
+    int curDig = vd[i];
     if (prevDigi == -1)
     {
-      prevDigi = vd[i];
+      prevDigi = curDig;
       continue; // skip. Nothing to compare
     }
 
-    if (prevDigi >= vd[i])
+    if (prevDigi >= curDig)
     {
-      if (prevDigi == vd[i])
-        isValid = true;
+      if (prevDigi == curDig)
+      {
+        if (freq_map.count(curDig) > 0)
+          freq_map[curDig] += 1;
+        else
+          freq_map[curDig] = 1;
+
+        isP1Val = true;
+      }
+
+      isAsc = true;
       prevDigi = vd[i];
     }
-    else {
-      isValid = false;
+    else // prev digit is larger, no longer valid
+    {
+      isP1Val = false;
+      isP2Val = false;
+      isAsc = false;
       break;
     }
   }
 
-  if (isValid)
-    pwCol.push_back(pw);
+  if (isAsc)
+  {
+    std::map<int,int>::iterator it = freq_map.begin();
+    while(it != freq_map.end())
+    {
+      int count = it->second;
+
+      if (count == 1) // 1 match of same key
+        isP2Val = true;
+      it++;
+    }
+  }
+
+  if (isP1Val)
+    p1Size++;
+  if (isP2Val)
+    p2Size++;
 }
 
 int main()
 {
   int sInt = { 123257 };
   int eInt = { 647015 };
-  std::vector<int> pwCol;
+  int p1Size = { 0 };
+  int p2Size = { 0 };
 
-  for (int n = sInt; n < eInt+ 1; n++)
+  for (int n = sInt; n < (eInt + 1); n++)
   {
-    validateNumPass(pwCol, n);
+    validateNumPass(p1Size, p2Size, n);
   }
 
   // p1
-  std::cout << pwCol.size() << std::endl;
+  std::cout << p1Size << std::endl;
+
+  // p2
+  std::cout << p2Size << std::endl;
+
   return 0;
 }
