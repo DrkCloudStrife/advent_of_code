fdb1d135e9e1f2dc01be46befb6601f252eb9229
Eduardo Garibay <dark.cloud2007@gmail.com>
2019-12-05 23:00:59 -0600

    Refactor intcode computer, part 1

--- 2019/intcode_computer.h
+++ 2019/intcode_computer.h
@@ -7,36 +7,36 @@
 class IntcodeComputer
 {
   private:
-    std::string file;
-    int m_noun;
-    int m_verb;
-    std::vector<int> v;
-    std::vector<int> vR;
+    int mNoun;
+    int mVerb;
+    std::vector<int> intCode;
+    std::vector<int> pIntCode;
     std::ifstream fs;
-    int vSize;
+    int pSize;
     int pMode;
+    void prepOperation(int&, int);
   protected:
-    static int haltCode;
-    static int addCode;
-    static int mulCode;
-    static int inCode;
-    static int outCode;
+    static const int haltCode;
+    static const int addCode;
+    static const int mulCode;
+    static const int inCode;
+    static const int outCode;
   public:
-    IntcodeComputer(std::string file);
+    IntcodeComputer(std::string);
 
-    void setInput(int noun, int verb);
+    void setInput(int, int);
     void run();
 
-    int getResult() { return vR[0]; }
-    int getNoun() { return m_noun; }
-    int getVerb() { return m_verb; }
+    int getResult() { return pIntCode[0]; }
+    int getNoun() { return mNoun; }
+    int getVerb() { return mVerb; }
 };
 
-int IntcodeComputer::haltCode = 99;
-int IntcodeComputer::addCode = 1;
-int IntcodeComputer::mulCode = 2;
-int IntcodeComputer::inCode = 3;
-int IntcodeComputer::outCode = 4;
+const int IntcodeComputer::haltCode = 99;
+const int IntcodeComputer::addCode = 1;
+const int IntcodeComputer::mulCode = 2;
+const int IntcodeComputer::inCode = 3;
+const int IntcodeComputer::outCode = 4;
 
 IntcodeComputer::IntcodeComputer(std::string file)
 {
@@ -51,66 +51,54 @@ IntcodeComputer::IntcodeComputer(std::string file)
   std::string s;
   while (std::getline(fs, s, ','))
   {
-    v.push_back(std::stoi(s));
+    intCode.push_back(std::stoi(s));
   }
   // Set defaults
-  m_noun = v[1];
-  m_verb = v[2];
-  vR = v;
-  vSize = v.size();
+  mNoun = intCode[1];
+  mVerb = intCode[2];
+  pIntCode = intCode;
+  pSize = pIntCode.size();
 }
 
 void IntcodeComputer::setInput(int noun, int verb)
 {
-  m_noun = noun;
-  m_verb = verb;
-  vR = v;
-  vR[1] = m_noun;
-  vR[2] = m_verb;
+  mNoun = noun;
+  mVerb = verb;
+  pIntCode = intCode;
+  pIntCode[1] = mNoun;
+  pIntCode[2] = mVerb;
+}
+
+void IntcodeComputer::prepOperation(int& opCode,int code)
+{
+  std::vector<int> params = splitInt(code);
+  opCode = code;
 }
 
 void IntcodeComputer::run()
 {
-  pMode = 0; // start as position mode
-  for(int i=0; i < vSize; i++)
+  for(int i=0; i < pSize; i++)
   {
-    int opCode = vR[i];
-    int n1     = vR[i+1];
-    int n2     = vR[i+2];
-    int t      = vR[i+3];
-    int v1     = vR[n1];
-    int v2     = vR[n2];
+    int opCode;
+    prepOperation(opCode, pIntCode[i]);
+    int n1     = pIntCode[i+1];
+    int n2     = pIntCode[i+2];
+    int t      = pIntCode[i+3];
+    int v1     = pIntCode[n1];
+    int v2     = pIntCode[n2];
 
-    if (opCode == haltCode)
-      break;
-
-    // Support parameter modes, assumptions made here
-   if (opCode > 100)
+    switch(opCode)
     {
-      std::vector<int> params = splitInt(opCode, true);
-      std::vector<int> m_op = {params.begin(), params.begin() + 2};
-
-      opCode = params
-      /* if (opCode == inCode) */
-      /* { */
-      /*   // TODO */
-      /* } */
-      /*  */
-      /* if (opCode == outCode) */
-      /* { */
-      /*   // TODO */
-      /* } */
+      case addCode:
+        pIntCode[t] = v1 + v2;
+      break;
+      case mulCode:
+        pIntCode[t] = v1 * v2;
+      break;
+      case haltCode:
+      default:
+      break;
     }
-
-
-    /* if (opCode == addCode) */
-    /* { */
-    /*   vR[t] = v1 + v2; */
-    /* } */
-    /* else (opCode == mulCode) */
-    /* { */
-    /*   vR[t] = v1 * v2; */
-    /* } */
 
     i += 3; // skip to next op
   }
