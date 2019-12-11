21cbc04e0b92fa483fc5c36b4187db34a2f25ad8
Eduardo Garibay <dark.cloud2007@gmail.com>
2019-12-10 20:46:57 -0600

    Solution for Day 6 part 1

--- 2019/day6.cpp
+++ 2019/day6.cpp
@@ -1,9 +1,42 @@
 #include "common.h"
 #include <map>
 
+struct Orbit
+{
+  std::string name;
+  struct Orbit* parent;
+};
+
+struct Orbit* CreateOrbit(std::string name)
+{
+  struct Orbit* pOrbit = new Orbit();
+  pOrbit->name = name;
+  pOrbit->parent = NULL;
+
+  return pOrbit;
+}
+
+typedef std::map<std::string, struct Orbit*> orbit_collection;
+
+struct Orbit* findOrCreateOrbit(orbit_collection& orbits, std::string name)
+{
+  struct Orbit* myOrbit;
+  if (orbits.count(name) > 0)
+  {
+    myOrbit = orbits.find(name)->second;
+  }
+  else
+  {
+    myOrbit = CreateOrbit(name);
+    orbits[name] = myOrbit;
+  }
+
+  return myOrbit;
+}
+
 int main()
 {
-  std::string fileName = "./day6_test.txt";
+  std::string fileName = "./day6.txt";
   std::ifstream fs;
   fs.open(fileName);
 
@@ -31,13 +64,43 @@ int main()
     lc += 1;
   }
 
-  for(int i=0; i < v.size(); i++)
-  {
-    for (auto& x: v[i])
-    {
+  // build orbit collection
+  orbit_collection orbits;
 
+  for (int i=0; i < v.size(); i++)
+  {
+    for (auto& o: v[i])
+    {
+      struct Orbit* parentOrbit = findOrCreateOrbit(orbits, o.first);
+      struct Orbit* childOrbit = findOrCreateOrbit(orbits, o.second);
+      childOrbit->parent = parentOrbit;
     }
   }
+
+  int dirOrbCount = { 0 };
+  int indirOrbCount = { 0 };
+
+  orbit_collection::iterator it = orbits.begin();
+
+  while (it != orbits.end())
+  {
+    struct Orbit* currentOrbit = it->second;
+    if (currentOrbit->parent != NULL)
+    {
+      dirOrbCount++;
+      while (currentOrbit->parent != NULL)
+      {
+        currentOrbit = currentOrbit->parent;
+        if (currentOrbit->parent != NULL)
+          indirOrbCount++;
+      }
+    }
+
+    it++;
+  }
+
+  // p1
+  std::cout << "D: " << dirOrbCount << "; I: " << indirOrbCount << "; T: " << dirOrbCount + indirOrbCount << std::endl;
 
   return 0;
 }
