bcb2db362756dcea56390ed921862b3272c6d593
Eduardo Garibay <dark.cloud2007@gmail.com>
2021-11-25 16:52:13 -0600

    Adds day 2 part 2 solution

--- 2020/python/02/main.py
+++ 2020/python/02/main.py
@@ -9,17 +9,39 @@ class PasswordValidator(object):
 
         for row in data:
             self.passwords.append(row)
+
         data.close()
 
-    def validate(self):
+    def validate_pt1(self):
+        if len(self.valid_passwords) != 0:
+            self.__reset_valid_passwords()
+
         for policy in self.passwords:
             p_range, p_character, p_password = self.__parse_policy(policy)
             pattern = re.compile(r'{}'.format(p_character))
-            range_arr = p_range.split('-')
-            range_1 = range(int(range_arr[0]), int(range_arr[1]) + 1)
+            range_arr = [int(n) for n in p_range.split('-')]
+            range_1 = range(range_arr[0], range_arr[1] + 1)
+
             if len(pattern.findall(p_password)) in range_1:
                 self.valid_passwords.append(p_password)
+
+    def validate_pt2(self):
+        if len(self.valid_passwords) != 0:
+            self.__reset_valid_passwords()
+
+        for policy in self.passwords:
+            p_range, p_character, p_password = self.__parse_policy(policy)
+            idx_1, idx_2 = [int(n) for n in p_range.split('-')]
+            char_1 = p_password[idx_1-1]
+            char_2 = p_password[idx_2-1]
+            if char_1 == char_2 or p_character not in [char_1, char_2]:
+                continue
+
+            self.valid_passwords.append(p_password)
 
     def __parse_policy(self, policy):
         results = re.findall(self.POLICY_PARSER, policy)
         return results[0]
+
+    def __reset_valid_passwords(self):
+        self.valid_passwords = []
--- 2020/python/02/test.py
+++ 2020/python/02/test.py
@@ -2,10 +2,15 @@ import os
 import main
 from main import PasswordValidator
 
-directory = os.getcwd()
-path = open("{dir}/{file}".format(dir=directory, file="spec/fixtures/2020_02_sample.txt"))
+path = os.path.dirname(os.path.realpath(__file__))
+path = open("{dir}/{file}".format(dir=path, file="../../../spec/fixtures/2020_02_sample.txt"))
 validator = PasswordValidator(path)
 
-validator.validate()
+validator.validate_pt1()
 
+print("Day 2 - Part 1 match?")
 print(len(validator.valid_passwords) == 2)
+
+validator.validate_pt2()
+print("Day 2 - Part 2 match?")
+print(len(validator.valid_passwords) == 1)
