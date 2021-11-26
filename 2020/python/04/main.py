aa67212b8a5b47301f0e30b36f2023935385d8d9
Eduardo Garibay <dark.cloud2007@gmail.com>
2021-11-26 13:22:09 -0600

    Creates class to pass test for day 4 pt1

--- 2020/python/04/main.py
+++ 2020/python/04/main.py
@@ -1,12 +1,14 @@
+import re
+
 class PasswordValidator(object):
     BIRTH_YEAR = 'byr:(\d+)(?=\s|$)'
-    ISSUE_YEAR = 'iyr:(\d+)(?=\s|$)'
-    EXPIRATION_YEAR = 'eyr:(\d+)(?=\s|$)'
-    HEIGHT = 'hgt:(\w+)(?=\s|$)'
-    HAIR_COLOR = 'hcl:(#\w+)(?=\s|$)'
-    EYE_COLOR = 'ecl:(\w+)(?=\s|$)'
-    PASSPORT_ID = 'pid:(\d+)(?=\s|$)'
     COUNTRY_ID = 'cid:(\d+)(?=\s|$)'
+    EXPIRATION_YEAR = 'eyr:(\d+)(?=\s|$)'
+    EYE_COLOR = 'ecl:(\w+)(?=\s|$)'
+    HAIR_COLOR = 'hcl:(#\w+)(?=\s|$)'
+    HEIGHT = 'hgt:(\w+)(?=\s|$)'
+    ISSUE_YEAR = 'iyr:(\d+)(?=\s|$)'
+    PASSPORT_ID = 'pid:(\d+)(?=\s|$)'
 
     def __init__(self, data):
         self.passports = []
@@ -16,7 +18,17 @@ class PasswordValidator(object):
     def validate_pt1(self):
         self.__reset_valid_passports()
         for passport in self.passports:
-            print(passport)
+            birth = self.__pattern('BIRTH_YEAR').findall(passport)
+            country = self.__pattern('COUNTRY_ID').findall(passport)
+            expiration = self.__pattern('EXPIRATION_YEAR').findall(passport)
+            eyes = self.__pattern('EYE_COLOR').findall(passport)
+            hair = self.__pattern('HAIR_COLOR').findall(passport)
+            height = self.__pattern('HEIGHT').findall(passport)
+            issued = self.__pattern('ISSUE_YEAR').findall(passport)
+            pid = self.__pattern('PASSPORT_ID').findall(passport)
+
+            if birth and expiration and eyes and hair and height and issued and pid:
+                self.valid_passports.append(passport)
 
     def __aggregate_passports(self, data):
         current_passport = ""
@@ -29,6 +41,10 @@ class PasswordValidator(object):
             current_passport += row.strip() + ' '
 
         self.passports.append(current_passport.strip())
+
+    def __pattern(self, pattern_type):
+        reg = getattr(self, pattern_type)
+        return re.compile(r'{}'.format(reg))
 
     def __reset_valid_passports(self):
         self.valid_passports = []
