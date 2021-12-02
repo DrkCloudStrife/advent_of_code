670a57e054ad013ee4cea30b7e46453e22cd9520
Eduardo Garibay <dark.cloud2007@gmail.com>
2021-12-01 20:29:05 -0600

    Adds AoC 2021 Day 1 pt2 solution

--- 2021/python/01/main.py
+++ 2021/python/01/main.py
@@ -8,19 +8,30 @@ class DepthMeasurement(object):
             self.sonar_depths.append(int(row.strip()))
 
     def pt1(self):
+        self.__count_depths(self.sonar_depths)
+
+    def pt2(self):
+        self.__reset_depth_counter()
+        tree_records = []
+        for idx, depth in enumerate(self.sonar_depths):
+            current_tree = self.sonar_depths[idx:idx+3]
+            if len(current_tree) == 3:
+                tree_records.append(reduce(lambda t1, t2: t1 + t2, current_tree))
+            else:
+                continue
+
+        self.__count_depths(tree_records)
+
+    def __reset_depth_counter(self):
+        self.depth_counter = 0
+
+    def __count_depths(self, data):
         prev_depth = None
         current_depth = None
-        for depth in self.sonar_depths:
+        for depth in data:
             prev_depth = current_depth
             current_depth = depth
             if prev_depth is None:
                 continue
             if prev_depth < current_depth:
                 self.depth_counter += 1
-
-    def pt2(self):
-        self.__reset_depth_counter()
-        print('TBD')
-
-    def __reset_depth_counter(self):
-        self.depth_counter = 0
--- 2021/python/01/test.py
+++ 2021/python/01/test.py
@@ -6,5 +6,9 @@ file_data = open("{dir}/{file}".format(dir=path, file="../../../spec/fixtures/20
 validator =  DepthMeasurement(file_data)
 
 print("Depth validator matches?")
-validator.validate_pt1()
+validator.pt1()
 print(validator.depth_counter == 7)
+
+print("Depth validator matches?")
+validator.pt2()
+print(validator.depth_counter == 5)
