c4a2c57f43347d98887ab3d38a0fa538c622de0f
Eduardo Garibay <dark.cloud2007@gmail.com>
2021-12-12 19:47:05 -0600

    Add method to print data

--- 2021/python/05/main.py
+++ 2021/python/05/main.py
@@ -1,13 +1,13 @@
 class Solution(object):
 
     def __init__(self, data):
+        self.max_covers = [0,0]
         self.__data = data
         self.lines = []
         self.__format_lines()
         self.__reset_counters()
 
     def pt1(self):
-        covers = {}
         counter = 0
         for line in self.lines:
             cover_x, cover_y = line
@@ -16,49 +16,73 @@ class Solution(object):
                 x = cover_x[0]
                 for ny in cover_y:
                     key = (x,ny)
-                    if key not in covers:
-                        covers[key] = 0
-                    covers[key] += 1
+                    if key not in self.covers:
+                        self.covers[key] = 0
+                    self.covers[key] += 1
             elif len(cover_y) == 1:
                 counter += 1
                 y = cover_y[0]
                 for nx in cover_x:
                     key = (nx,y)
-                    if key not in covers:
-                        covers[key] = 0
-                    covers[key] += 1
+                    if key not in self.covers:
+                        self.covers[key] = 0
+                    self.covers[key] += 1
 
-        self.counter = self.__get_overlap_count(covers)
+        self.__print_covers()
+        self.counter = self.__get_overlap_count()
 
     def pt2(self):
         self.__reset_counters()
         print("TBD")
 
     def __format_lines(self):
+        max_x = 0
+        max_y = 0
         for line in self.__data:
             input_1, input_2 = line.split(' -> ')
             x1, y1 = input_1.split(',')
             x2, y2 = input_2.split(',')
             if x1 > x2:
                 cover_x = range(int(x2), int(x1)+1)
+                if x1 > max_x: max_x = x1
             else:
                 cover_x = range(int(x1), int(x2)+1)
+                if x2 > max_x: max_x = x2
             if y1 > y2:
                 cover_y = range(int(y2), int(y1)+1)
+                if y1 > max_y: max_y = y1
             else:
                 cover_y = range(int(y1), int(y2)+1)
+                if y2 > max_y: max_y = y2
             self.lines.append([cover_x, cover_y])
+        self.max_covers = [max_x, max_y]
 
     def __reset_counters(self):
         self.counter = 0
         self.overlaps = []
+        self.covers = {}
 
-    def __get_overlap_count(self, covers):
+    def __get_overlap_count(self):
         counter = 0
 
-        for (coords, count) in covers.items():
+        for (coords, count) in self.covers.items():
             if count > 1:
                 counter += 1
 
         return counter
 
+    def __print_covers(self):
+        visual_covers = ""
+
+        for y in range(0, int(self.max_covers[1])+1):
+            visual_row = ""
+            for x in range(0, int(self.max_covers[0])+1):
+                key = (x, y)
+                if key in self.covers:
+                    visual_row += str(self.covers[key])
+                else:
+                    visual_row += '.'
+
+            visual_covers += "{}\n".format(visual_row)
+
+        print(visual_covers)
