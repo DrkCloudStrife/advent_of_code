31d633c8d77a529da58a855871078df9ec22788d
Eduardo Garibay <dark.cloud2007@gmail.com>
2021-12-27 21:52:18 -0600

    Adds Day 7 pt 1 solution

--- 2021/python/07/main.py
+++ 2021/python/07/main.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 class Solution(object):
 
     def __init__(self, data):
@@ -5,10 +7,17 @@ class Solution(object):
         self.__reset_counters()
 
         for row in data:
-            self.data.append(row.strip())
+            [self.data.append(int(n)) for n in row.split(',')]
 
-    def pt1(self):
-        print("TBD")
+    def pt1(self, most_common = None):
+        if most_common is None:
+            counter = Counter(self.data)
+            movements = {}
+            for common in counter.most_common():
+                movements[common[0]] = self.__solve_for(common[0])
+            self.counter = min(movements.values())
+        else:
+            self.counter = self.__solve_for(most_common)
 
     def pt2(self):
         self.__reset_counters()
@@ -16,3 +25,16 @@ class Solution(object):
 
     def __reset_counters(self):
         self.counter = 0
+
+    def __solve_for(self, position):
+        movements = 0
+
+        for h_pos in self.data:
+            if h_pos > position:
+                # subtract to position
+                movements += (h_pos - position)
+            elif h_pos < position:
+                # add to position
+                movements += (position - h_pos)
+
+        return movements
